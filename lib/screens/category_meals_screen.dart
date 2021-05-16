dfb6f331a3a81956ab844e04a8400f66e7ab638a
Yasmeen700 <b7Xy8e5XUZPABAAIrO31nUQPcCnWDIoB>
2021-05-16 17:07:35 +0300

    add meal model and meal item

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+import '../dummy_data.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category_meals';
+
+  // final String id;
+  // final String title;
+  // CategoryMealsScreen({this.title,this.id});
+  @override
+  Widget build(BuildContext context) {
+    final routeArg =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final catTitle = routeArg['title'];
+    final catId = routeArg['id'];
+    final catMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(catId);
+    }).toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          catTitle,
+        ),
+      ),
+      body:
+          // if(catMeals != null && catMeals.length >0){
+          FutureBuilder(
+        builder: (ctx, snapshot) {
+          if (catMeals != null && catMeals.length > 0) {
+            return ListView.builder(
+              itemBuilder: (ctx, index) {
+                return MealItem(
+                  title: catMeals[index].title,
+                  duration: catMeals[index].duration,
+                  affordability: catMeals[index].affordability,
+                  complexity: catMeals[index].complexity,
+                  imageUrl: catMeals[index].imageUrl,
+                );
+              },
+              itemCount: catMeals.length,
+            );
+          } else
+            return CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
